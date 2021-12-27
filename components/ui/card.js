ddb71c16fcd0d6bc9cf057fe46e320212c8c570d
xencodes <xencodes@icloud.com>
2021-12-27 21:42:36 +0530

    light gray card bg

--- components/ui/card.js
+++ components/ui/card.js
@@ -3,7 +3,7 @@ import { Box, Divider, Heading, Stack, Text } from '@chakra-ui/react';
 
 function Card({ children, title, subTitle }) {
 	return (
-		<Box px={4} py={3} bg='gray.100' rounded='md'>
+		<Box px={4} py={3} bg='gray.50' rounded='md'>
 			<Heading size='md'>{title}</Heading>
 			<Text fontSize={'sm'} color='gray.500'>
 				{subTitle}
