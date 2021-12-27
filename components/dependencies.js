a397e899c86099aa9f560fd6a508397f2827cd74
xencodes <xencodes@icloud.com>
2021-12-27 21:56:26 +0530

    deps and devdeps

--- components/dependencies.js
+++ components/dependencies.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Tag, HStack, Spacer, Stack, Text } from '@chakra-ui/react';
 import Card from './ui/card';
 
-function Dependencies() {
-	return <Card title='Dependencies'></Card>;
+function Dependencies({ deps, devDeps }) {
+	const depsKeys = Object.keys(deps);
+	const devDepsKeys = Object.keys(devDeps);
+
+	return (
+		<Stack>
+			<Card title='Dependencies'>
+				{depsKeys.map((key, index) => (
+					<HStack key={index}>
+						<Text>{key}</Text>
+						<Spacer />
+						<Tag>{deps[key]}</Tag>
+					</HStack>
+				))}
+			</Card>
+			<Card title='Dev Dependencies'>
+				{devDepsKeys.map((key, index) => (
+					<HStack key={index}>
+						<Text>{key}</Text>
+						<Spacer />
+						<Tag>{devDeps[key]}</Tag>
+					</HStack>
+				))}
+			</Card>
+		</Stack>
+	);
 }
 
 export default Dependencies;
