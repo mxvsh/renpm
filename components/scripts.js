808ec7047a75af9386620fd30e106c56f3c9a1cf
xencodes <xencodes@icloud.com>
2021-12-28 20:40:05 +0530

    fix script component

--- components/scripts.js
+++ components/scripts.js
@@ -7,65 +7,99 @@ import {
 	Stack,
 	Spacer,
 	Text,
+	ButtonGroup,
 } from '@chakra-ui/react';
-import { FiPlay } from 'react-icons/fi';
+import { FiBook, FiPlay } from 'react-icons/fi';
 import Card from './ui/card';
 import { socket } from '../providers/socket.io';
 
-function Scripts({ name, scripts }) {
+function Scripts({ name, scripts, processes = [] }) {
+	const scriptProcesses = processes.filter(
+		({ name: pkgName }) => pkgName === name
+	);
+
 	const scriptKeys = useMemo(() => Object.keys(scripts), [scripts]);
 	const [running, setRunning] = useState({});
 
 	useEffect(() => {
-		socket.on('log', (msg) => {
-			console.log(msg);
+		scriptProcesses.map((p) => {
+			setRunning({
+				...running,
+				[p.script]: true,
+			});
 		});
+
+		socket.on('stopped', (data) => {
+			if (data.name === name) {
+				setRunning((prev) => ({
+					...prev,
+					[data.script]: false,
+				}));
+			}
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	const run = (script) => {
-		setRunning({ ...running, [name]: true });
+		setRunning({ ...running, [script]: true });
 		socket.emit('run', {
 			script,
 			name,
 		});
-		setTimeout(() => {
-			setRunning({ ...running, [name]: false });
-		}, 2000);
+	};
+
+	const stop = (script) => {
+		socket.emit('stop', {
+			script,
+			name,
+		});
+		setRunning({ ...running, [script]: false });
 	};
 
 	return (
 		<Card title='Scripts' subTitle='Execute scripts from the package'>
 			<Stack userSelect={'none'}>
-				{scriptKeys.map((key, index) => (
-					<div key={index}>
-						<HStack
-							willChange={''}
-							transition={'all 0.2s'}
-							_hover={{
-								transform: 'translateX(2px)',
-							}}
-						>
-							<Box fontSize={'sm'}>
-								<Text fontWeight={'semibold'}>{key}</Text>
-								<Text>{scripts[key]}</Text>
-							</Box>
-							<Spacer />
-							<Button
-								variant={'ghost'}
-								leftIcon={<FiPlay />}
-								size='xs'
-								colorScheme={'red'}
-								isLoading={running[key]}
-								onClick={() => {
-									run(key);
-								}}
-							>
-								run
-							</Button>
-						</HStack>
-						{index < scriptKeys.length - 1 && <Divider mt={2} />}
-					</div>
-				))}
+				{scriptKeys.map((script, index) => {
+					const isRunning = running[script];
+
+					return (
+						<div key={index}>
+							<HStack>
+								<Box fontSize={'sm'}>
+									<Text fontWeight={'semibold'}>{script}</Text>
+									<Text>{scripts[script]}</Text>
+								</Box>
+								<Spacer />
+								<ButtonGroup size='xs'>
+									<Button
+										variant={'ghost'}
+										leftIcon={<FiBook />}
+										onClick={() => {}}
+									>
+										view logs
+									</Button>
+									<Button
+										variant={'ghost'}
+										bg={isRunning ? 'red.50' : 'green.50'}
+										borderWidth={'1px'}
+										borderColor={'transparent'}
+										_hover={{
+											borderColor: isRunning ? 'red.300' : 'green.300',
+										}}
+										leftIcon={<FiPlay />}
+										colorScheme={isRunning ? 'red' : 'green'}
+										onClick={() => {
+											isRunning ? stop(script) : run(script);
+										}}
+									>
+										{isRunning ? 'stop' : 'run'}
+									</Button>
+								</ButtonGroup>
+							</HStack>
+							{index < scriptKeys.length - 1 && <Divider mt={2} />}
+						</div>
+					);
+				})}
 			</Stack>
 		</Card>
 	);
