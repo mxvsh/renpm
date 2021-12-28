9c622558b003840adfe476a16752b9dca6efb2d5
xencodes <xencodes@icloud.com>
2021-12-28 16:35:17 +0530

    add socket io

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+const { spawn } = require('child_process');
 const express = require('express');
 const next = require('next');
-
-const dev = process.env.NODE_ENV === 'development';
+const config = require('./providers/config');
+const dev = process.argv[2] === 'dev' || process.env.NODE_ENV === 'development';
 const app = next({ dev, dir: __dirname });
 const handle = app.getRequestHandler();
 
@@ -11,11 +12,35 @@ function main(PORT) {
 		.then(() => {
 			const server = express();
 
+			const http = require('http').createServer(server);
+			const io = require('socket.io')(http);
+
+			io.on('connection', (socket) => {
+				socket.on('run', (data) => {
+					const { script, name } = data;
+					const packages = config.get('packages');
+					const pkg = packages.find(({ name: pkgName }) => pkgName === name);
+					const child = spawn('npm', ['run', script], {
+						cwd: pkg.__cwd,
+					});
+					child.stdout.setEncoding('utf8');
+					child.stdout.on('data', (data) => {
+						socket.emit('log', data);
+					});
+					child.stderr.on('data', (data) => {
+						socket.emit('log', data);
+					});
+					child.on('close', (code) => {
+						socket.emit('log', `child process exited with code ${code}`);
+					});
+				});
+			});
+
 			server.get('*', (req, res) => {
 				return handle(req, res);
 			});
 
-			server.listen(PORT, (err) => {
+			http.listen(PORT, (err) => {
 				if (err) throw err;
 				console.log('> Ready on http://localhost:' + PORT);
 			});
