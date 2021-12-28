2ae084b35573fd4cc96d38ad09e40e18ce24b658
xencodes <xencodes@icloud.com>
2021-12-28 16:34:33 +0530

    add view mode

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import { Box, Divider, Heading, Stack, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+	Box,
+	Divider,
+	Heading,
+	HStack,
+	IconButton,
+	Fade,
+	Spacer,
+	Stack,
+	Text,
+} from '@chakra-ui/react';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
 function Card({ children, title, subTitle }) {
+	const [view, setView] = useState(true);
+
 	return (
 		<Box px={4} py={3} bg='gray.50' rounded='md'>
-			<Heading size='md'>{title}</Heading>
+			<HStack>
+				<Heading size='md'>{title}</Heading>
+				<Spacer />
+				<IconButton
+					icon={view ? <FiEyeOff /> : <FiEye />}
+					variant={'ghost'}
+					onClick={() => {
+						setView(!view);
+					}}
+				/>
+			</HStack>
 			<Text fontSize={'sm'} color='gray.500'>
 				{subTitle}
 			</Text>
-			<Divider my={4} />
-			<Stack px={2}>{children}</Stack>
+			<Fade in={view} unmountOnExit>
+				<Divider my={4} />
+				<Stack px={2}>{children}</Stack>
+			</Fade>
 		</Box>
 	);
 }
