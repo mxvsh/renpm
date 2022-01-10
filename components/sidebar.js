6379515c2ee8f66b24807fb90432842b1d043618
ajstudd <j7654894110@gmail.com>
2022-01-10 20:05:15 +0530

    rounded sidebar props

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,59 +1,59 @@
-import React from 'react';
-import { Box, Divider, Heading, HStack, Stack, Text } from '@chakra-ui/react';
-import { FiCoffee, FiSettings } from 'react-icons/fi';
+import React from "react";
+import { Box, Divider, Heading, HStack, Stack, Text } from "@chakra-ui/react";
+import { FiCoffee, FiSettings } from "react-icons/fi";
 
 const Options = [
-	['Donate', <FiCoffee key={2} />, '/settings'],
-	['Settings', <FiSettings key={1} />, '/donate'],
+  ["Donate", <FiCoffee key={2} />, "/settings"],
+  ["Settings", <FiSettings key={1} />, "/donate"],
 ];
 
 const SidebarMenu = () => {
-	return (
-		<Stack>
-			{Options.map(([label, icon], idx) => (
-				<HStack
-					key={idx}
-					p={2}
-					rounded='lg'
-					cursor={'pointer'}
-					_hover={{ color: 'red.400', bg: 'red.50' }}
-					transition={'all 0.3s'}
-				>
-					{icon}
-					<Text>{label}</Text>
-				</HStack>
-			))}
-		</Stack>
-	);
+  return (
+    <Stack>
+      {Options.map(([label, icon], idx) => (
+        <HStack
+          key={idx}
+          p={2}
+          rounded="2xl"
+          cursor={"pointer"}
+          _hover={{ color: "red.400", bg: "red.50" }}
+          transition={"all 0.3s"}
+        >
+          {icon}
+          <Text>{label}</Text>
+        </HStack>
+      ))}
+    </Stack>
+  );
 };
 
 const ProjectItem = ({ title, description }) => (
-	<Box cursor={'pointer'}>
-		<Heading size='md' fontWeight={'600'}>
-			{title}
-		</Heading>
-		<Text fontSize={'sm'} noOfLines={2}>
-			{description || 'No description found'}
-		</Text>
-	</Box>
+  <Box cursor={"pointer"}>
+    <Heading size="md" fontWeight={"600"}>
+      {title}
+    </Heading>
+    <Text fontSize={"sm"} noOfLines={2}>
+      {description || "No description found"}
+    </Text>
+  </Box>
 );
 
 function Sidebar({ packages = [], onChange }) {
-	return (
-		<Stack spacing={4} userSelect={'none'}>
-			<Box h='xs' overflow={'auto'}>
-				<Stack spacing={4}>
-					{packages.map((pkg, idx) => (
-						<div key={idx} onClick={() => onChange(pkg)}>
-							<ProjectItem title={pkg.name} description={pkg.description} />
-						</div>
-					))}
-				</Stack>
-			</Box>
-			<Divider />
-			<SidebarMenu />
-		</Stack>
-	);
+  return (
+    <Stack spacing={4} userSelect={"none"}>
+      <Box h="xs" overflow={"auto"}>
+        <Stack spacing={4}>
+          {packages.map((pkg, idx) => (
+            <div key={idx} onClick={() => onChange(pkg)}>
+              <ProjectItem title={pkg.name} description={pkg.description} />
+            </div>
+          ))}
+        </Stack>
+      </Box>
+      <Divider />
+      <SidebarMenu />
+    </Stack>
+  );
 }
 
 export default Sidebar;
